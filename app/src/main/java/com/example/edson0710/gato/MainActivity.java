5e624fb04bb3b6770601c2b351c1143ef7d83c50
Edson0710 <edsonnavarro0710@gmail.com>
2018-05-22 22:15:09 -0500

    Se añaden los turnos y metodo para ver quién ganó

--- app/src/main/java/com/example/edson0710/gato/MainActivity.java
+++ app/src/main/java/com/example/edson0710/gato/MainActivity.java
@@ -52,7 +52,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if(!((Button) v).getText().toString().equals(""))
+        if(!((Button) v).getText().toString().equals("")){
+            return;
+        }
+
+        if(turno1){
+            ((Button)v).setText("X");
+        } else{
+            ((Button)v).setText("O");
+        }
+
+        rondascont++;
+
+        if(Ganador()){
+            if(turno1){
+                gano1();
+            } else{
+                gano2();
+            }
+        } else if(rondascont == 9){
+            draw();
+        } else{
+            turno1 = !turno1;
+        }
     }
+
+    private boolean Ganador(){
+        String[][] field = new String[3][3];
+
+        for(int i = 0;i < 3; i++) {
+            for(int j = 0;j < 3; j++){
+                field[i][j] = botones[i][j].getText().toString();
+            }
+        }
+
+        for(int i = 0;i < 3;i++){
+            if(field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")){
+                return true;
+            }
+        }
+
+        for(int i = 0;i < 3;i++){
+            if(field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")){
+                return true;
+            }
+        }
+
+        if(field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")){
+            return true;
+        }
+
+        if(field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")){
+            return true;
+        }
+
+        return false;
+    }
+
+    private void gano1(){}
+
+    private void gano2(){}
+
+    private void draw() {}
 }
 
